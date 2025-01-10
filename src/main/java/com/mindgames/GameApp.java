e004183b5933fc2253c94dd181b78f6d2d7f67a7
eric carpo <carpoeric@gmail.com>
2025-01-09 22:02:14 -0800

    API integration and code clean

--- src/main/java/com/mindgames/GameApp.java
+++ src/main/java/com/mindgames/GameApp.java
@@ -1,41 +1,48 @@
 package com.mindgames;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
 
-public class GameApp
-{
+public class GameApp {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
     public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
     public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
 
     public static void mainMenu() {
-    System.out.println();
-    System.out.println("\u001B[32mWelcome to MINDGAMES!\u001B[0m");
-    System.out.println("You will have 10 tries to figure out the number combination I'm thinking of.");
-    System.out.println("Each guess only consists of the digits 0-7 (no using 8 or 9!). Hit the enter button to submit.");
-    System.out.println("Once you've made a guess, I'll give you some hints to help you figure out the correct combination.");
-    System.out.println("\nHere's what the hints mean:");
-    System.out.println(ANSI_GREEN_BACKGROUND + " 0 " + ANSI_YELLOW_BACKGROUND + " 1 " + ANSI_BLACK_BACKGROUND + " 2 " + " 3 " + ANSI_RESET);
-    System.out.println("Number " + ANSI_GREEN_BACKGROUND + " 0 " + ANSI_RESET + " is in the combo and in the correct spot.");
-    System.out.println("Number " + ANSI_YELLOW_BACKGROUND + " 1 " + ANSI_RESET + " is in the combo but in the wrong spot.");
-    System.out.println("Numbers " + ANSI_BLACK_BACKGROUND + " 2 " + ANSI_RESET + "AND " + ANSI_BLACK_BACKGROUND + " 3 " + ANSI_RESET  + "are not in the combo at all.");
-    System.out.println("\nIf you're up for the challenge, please drop the number 1 to start:");
+        System.out.println();
+        System.out.println("\u001B[32mWelcome to MINDGAMES!\u001B[0m");
+        System.out.println("You will have 10 tries to figure out the four digit number combination I am thinking of.");
+        System.out.println("Each guess should consist of any digits from 0-7 (none of my combos use 8 or 9!). Hit the enter button to submit.");
+        System.out.println("Once you've made a guess, I will give you some hints to help you figure out the correct combination.");
+        System.out.println("\nHere's what the hints look like:");
+        System.out.println(ANSI_GREEN_BACKGROUND + " 0 " + ANSI_YELLOW_BACKGROUND + " 1 " + ANSI_BLACK_BACKGROUND + " 2 " + " 3 " + ANSI_RESET);
+        System.out.println("Number " + ANSI_GREEN_BACKGROUND + " 0 " + ANSI_RESET + " is in the combo and in the correct spot.");
+        System.out.println("Number " + ANSI_YELLOW_BACKGROUND + " 1 " + ANSI_RESET + " is in the combo but in the wrong spot.");
+        System.out.println("Number " + ANSI_BLACK_BACKGROUND + " 2 " + ANSI_RESET + " & Number " + ANSI_BLACK_BACKGROUND + " 3 " + ANSI_RESET  + " are not in the combo at all.");
+        System.out.println("\nIf you're up for the challenge, please enter the number 1 to start:");
     }
 
-    public static String[] randomNumber() {
-    // TODO: API calls ----- https://www.random.org/integers
-    String path = "src/main/resources/4digitcombos0to7.txt";
-    String contents = null;
-    try {
-        contents = new String(Files.readAllBytes(Paths.get(path)));
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-    assert contents != null;
-    return contents.split("\\r?\\n");
-}
-}
+    public static String randomNumber() {
+        String apiUrl = "https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new";
+        StringBuilder randomNumbers = new StringBuilder();
 
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            Scanner scanner = new Scanner(connection.getInputStream());
+            while (scanner.hasNextLine()) {
+                randomNumbers.append(scanner.nextLine().trim());
+            }
+            scanner.close();
+        } catch (IOException e) {
+            System.err.println("Error fetching random numbers: " + e.getMessage());
+        }
+
+        return randomNumbers.toString();
+    }
+}
--- src/main/java/com/mindgames/Main.java
+++ src/main/java/com/mindgames/Main.java
@@ -3,89 +3,69 @@ package com.mindgames;
 import java.util.*;
 import static com.mindgames.GameApp.*;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        com.mindgames.GameApp.mainMenu();
+public class Main {
+    public static void main(String[] args) {
+        mainMenu();
 
         Scanner scanner = new Scanner(System.in);
         String scanInput = scanner.nextLine().trim();
 
         while (scanInput.equals("1")) {
             String numberInput;
-            String[] numberArray = com.mindgames.GameApp.randomNumber();
-            List<String> numberList = Arrays.asList(numberArray);
-            Random random = new Random();
-            String randomNumber = numberList.get(random.nextInt(numberList.size()));
+            String randomNumber = randomNumber();
             char[] arrayRandomNumber = randomNumber.toCharArray();
             System.out.println("\nGUESSING TIME!\nPlease type your first guess: ");
 
             int tries = 1;
             while (tries <= 10) {
                 numberInput = scanner.nextLine().trim();
-                while ((numberInput.length() != 4) || (!numberList.contains(numberInput)))
-                {
+                while (numberInput.length() != 4 || !numberInput.matches("[0-7]+")) {
                     System.out.print("Invalid guess... Give it another go: ");
                     numberInput = scanner.nextLine().trim();
                 }
                 char[] arrayNumberTyped = numberInput.toCharArray();
 
                 System.out.print("Guess " + tries + "/10: ");
-                if (Arrays.equals(arrayNumberTyped, arrayRandomNumber))
-                {
-                    for (int i = 0; i < 4; i++)
-                    {
+                if (Arrays.equals(arrayNumberTyped, arrayRandomNumber)) {
+                    for (int i = 0; i < 4; i++) {
                         System.out.print(ANSI_GREEN_BACKGROUND + " " + arrayNumberTyped[i] + " " + ANSI_RESET);
                     }
                     System.out.println("\n\nAwesome! You solved that in \u001B[32m" + tries + "\u001B[0m tries.");
-                    tries = 10;
-
+                    break;
                 } else {
-                    for (int i = 0; i < 4; i++)
-                    {
-
+                    for (int i = 0; i < 4; i++) {
                         int countCharTyped = numberInput.length() - numberInput.replaceAll(String.valueOf(arrayNumberTyped[i]), "").length();
                         int countCharRandom = randomNumber.length() - randomNumber.replaceAll(String.valueOf(arrayNumberTyped[i]), "").length();
                         int colorBlocks = countCharRandom;
 
-                        if (randomNumber.contains(String.valueOf(arrayNumberTyped[i])))
-                        {
-                            if (arrayNumberTyped[i] == arrayRandomNumber[i])
-                            {
+                        if (randomNumber.contains(String.valueOf(arrayNumberTyped[i]))) {
+                            if (arrayNumberTyped[i] == arrayRandomNumber[i]) {
                                 System.out.print(ANSI_GREEN_BACKGROUND + " " + arrayNumberTyped[i] + " " + ANSI_RESET);
                             } else {
-                                if (countCharTyped <= countCharRandom)
-                                {
+                                if (countCharTyped <= countCharRandom) {
                                     System.out.print(ANSI_YELLOW_BACKGROUND + " " + arrayNumberTyped[i] + " " + ANSI_RESET);
                                 } else {
-                                    for (int j=0; j<=i; j++)
-                                    {
-                                        if (arrayNumberTyped[j]==arrayNumberTyped[i]) colorBlocks--;
+                                    for (int j = 0; j <= i; j++) {
+                                        if (arrayNumberTyped[j] == arrayNumberTyped[i]) colorBlocks--;
                                     }
-                                    for (int k=0; k<4; k++)
-                                    {
-                                        if (arrayNumberTyped[k]==arrayRandomNumber[i]) colorBlocks--;
+                                    for (int k = 0; k < 4; k++) {
+                                        if (arrayNumberTyped[k] == arrayRandomNumber[i]) colorBlocks--;
                                     }
-                                    if (colorBlocks>0)
-                                    {
+                                    if (colorBlocks > 0) {
                                         System.out.print(ANSI_YELLOW_BACKGROUND + " " + arrayNumberTyped[i] + " " + ANSI_RESET);
                                     } else {
-                                        if (countCharRandom>0)
-                                        {
+                                        if (countCharRandom > 0) {
                                             int checkPreviouslyShown = 0;
                                             int countGreenBlocks = 0;
-                                            for (int j=0; j<i; j++)
-                                            {
-                                                if (arrayNumberTyped[j]==arrayNumberTyped[i]){
+                                            for (int j = 0; j < i; j++) {
+                                                if (arrayNumberTyped[j] == arrayNumberTyped[i]) {
                                                     checkPreviouslyShown++;
                                                 }
                                             }
-                                            for (int k=0; k<4; k++)
-                                            {
-                                                if (arrayNumberTyped[k]==arrayRandomNumber[k] && arrayNumberTyped[i]==arrayNumberTyped[k]) countGreenBlocks++;
+                                            for (int k = 0; k < 4; k++) {
+                                                if (arrayNumberTyped[k] == arrayRandomNumber[k] && arrayNumberTyped[i] == arrayNumberTyped[k]) countGreenBlocks++;
                                             }
-                                            if (checkPreviouslyShown==0 && countGreenBlocks==0) System.out.print(ANSI_YELLOW_BACKGROUND + " " + arrayNumberTyped[i] + " " + ANSI_RESET);
+                                            if (checkPreviouslyShown == 0 && countGreenBlocks == 0) System.out.print(ANSI_YELLOW_BACKGROUND + " " + arrayNumberTyped[i] + " " + ANSI_RESET);
                                             else System.out.print(ANSI_BLACK_BACKGROUND + " " + arrayNumberTyped[i] + " " + ANSI_RESET);
                                         } else {
                                             System.out.print(ANSI_BLACK_BACKGROUND + " " + arrayNumberTyped[i] + " " + ANSI_RESET);
@@ -105,13 +85,12 @@ public class Main
                 tries++;
             }
             System.out.print("The correct combination is... ");
-            for (int i=0; i<4; i++){
+            for (int i = 0; i < 4; i++) {
                 System.out.print(ANSI_GREEN_BACKGROUND + " " + arrayRandomNumber[i] + " " + ANSI_RESET);
             }
 
             System.out.println("\n\nEnter 1 to go again!\nEnter 0 to exit.");
             scanInput = scanner.nextLine().trim();
-
         }
 
         System.out.println("Seems like something went wrong. Exiting program!");
